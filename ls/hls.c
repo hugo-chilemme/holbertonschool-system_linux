f2a6c02c0357d07e90e678bb4ff939889004ce8b
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-08-02 08:41:44 +0000

    Adding argument in a command line 😁

--- ls/hls.c
+++ ls/hls.c
@@ -3,19 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * main - Function
- * Return: -1 for error, 0 for success
- */
-int main(void)
+void showDirectory(char *path)
 {
 	struct dirent *entry;
 	DIR *dir;
 
-	dir = opendir(".");
+	dir = opendir(path);
 
 	if (!dir)
-		return (-1);
+		return;
 
 	while ((entry = readdir(dir)))
 	{
@@ -24,10 +20,38 @@ int main(void)
 		printf("%s  ", entry->d_name);
 	}
 
-	printf("\n");
-
 	free(entry);
 	free(dir);
+
+	printf("\n");
+}
+
+/**
+ * main - Function
+ * Return: -1 for error, 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int argv_i = 1;
+
+	if (argc > 1)
+	{
+		for (; argc > 1 && argv_i < argc; argv_i++)
+		{
+			// Display the path of repertory when he have multiple 
+			if (argc > 2)
+				printf("%s:\n", argv[argv_i]);
+
+			showDirectory(argv[argv_i]);
+
+			// Display another backspace when he have multiple repository
+			if (argv_i != argc - 1)
+				printf("\n");
+		}
+		return (0);
+	}
+
+	showDirectory(".");
 
 	return (0);
 }
