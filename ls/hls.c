60eb9b0164c1d6c0c59dea3143df6ede63eaa212
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-08-02 09:23:22 +0000

    Adding error message 🪐

--- ls/hls.c
+++ ls/hls.c
@@ -2,14 +2,33 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+void checkPath(char *program_name, char *path)
+{
+	struct stat buffer;
+	int status;
 
+	status = lstat(path, &buffer);
+
+	if (status == -1)
+	{
+		fprintf(stderr, "%s: cannot access %s: No such file or directory\n", program_name, path);
+		return;
+	}
+	if (access(path, R_OK) != 0)
+	{
+		fprintf(stderr, "%s: cannot open directory %s: Permission denied\n", program_name, path);
+		return;
+	}
+}
 /**
  * showDirectory - Display all folders and file a repository
  * @path: the target repository
  * Return: error code, 0 success
  */
-void showDirectory(char *program_name, char *path, int showPath, int backspace)
+void showDirectory(char *path, int showPath, int backspace)
 {
 	struct dirent *entry;
 	DIR *dir;
@@ -17,11 +36,8 @@ void showDirectory(char *program_name, char *path, int showPath, int backspace)
 
 	dir = opendir(path);
 
-	if (!dir)
-	{
-		fprintf(stderr, "%s: cannot access %s: No such file or directory\n", program_name, path);
+	if (!dir) 
 		return;
-	}
 
 	if (showPath)
 		printf("%s:\n", path);
@@ -57,12 +73,13 @@ int main(int argc, char *argv[])
 	{
 		for (; argc > 1 && argv_i < argc; argv_i++)
 		{
-			showDirectory(program_name, argv[argv_i], argc > 2, argv_i != argc - 1);
+			checkPath(program_name, argv[argv_i]);
+			showDirectory(argv[argv_i], argc > 2, argv_i != argc - 1);
 		}
 		return (0);
 	}
 
-	showDirectory(program_name, ".", 0, 0);
+	showDirectory(".", 0, 0);
 
 	return (0);
 }
