b16b9f5f0c0ee51fdf526906ad44fa7397783b8f
Hugo Chilemme <hugo.chilemme@gmail.com>
2024-06-26 14:58:44 +0200

    Fix bug memory jump with lstat

--- ls/listFiles.c
+++ ls/listFiles.c
@@ -12,19 +12,20 @@ int listFiles(char *path, char *execn, const int MUL_ARGS)
 {
 	DIR *dir;
 	struct dirent *entry;
-    struct stat file_info;
+	struct stat file_info;
 	const char *M_ERR_FNF = "No such file or directory";
 
-	memset(&file_info, 0, sizeof(struct stat));
+	if (path == NULL)
+	{
+		fprintf(stderr, "%s: cannot access %s: %s\n", execn, path, M_ERR_FNF);
+		return (0);
+	}
 
-	lstat(path, &file_info);
-
-	if (S_ISREG(file_info.st_mode))
+	if (stat(path, &file_info) == -1 || S_ISREG(file_info.st_mode))
 	{
 		printf("%s\n", path);
 		return (1);
 	}
-
 
 	dir = opendir(path);
 	if (dir == NULL)
